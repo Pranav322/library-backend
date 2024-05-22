49cf62521ab05b62717de50dbfa414349c4b2011
Pranav kumar <pranav88096@gmail.com>
2024-05-23 04:08:03 +0530

    new updates for heroku deployment

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-4qzj1%#dh+oyv@i=zznvarggr2@5ds_*6e3ud6=o8b4(*g0nyv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
+
 
 
 # Application definition
@@ -36,19 +37,44 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    "whitenoise.runserver_nostatic",
     'django.contrib.staticfiles',
+    "rest_framework",
+    "corsheaders",
     "todos.apps.TodosConfig",
 ]
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / "static"] # new
+STATICFILES_STORAGE ="whitenoise.storage.CompressedManifestStaticFilesStorage" # new
+
+
+CORS_ALLOWED_ORIGINS = (
+"http://localhost:3000",
+"https://localhost:5173",
+"http://localhost:8000",
+)
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000", "http://localhost:5173"]
+
 
 ROOT_URLCONF = 'django_project.urls'
 
